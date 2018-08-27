9e71369f4c88ca06b7c712960f5d433d8fd01ddf
Felipe Motta <felipe.motta@roche.com>
2018-08-27 22:48:17 +0200

    Update project structure Add testing and performance framework Rename License file

---
+++ tests/Assembly.Standard.Performance.Tests/CodeOnEvaluationPerformanceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace MesuredAssembly
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CodeOnEvaluation
+    {
+        [TestMethod]
+        public void PerformanceTest()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
